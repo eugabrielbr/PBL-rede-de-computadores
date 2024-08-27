2bc4cbc4566c3425a236543de20977e9e9b5cd58
Gabriel <gabrielnetbrine@gmail.com>
2024-08-27 11:39:47 -0300

    adicionando diagrama

--- metas/sessao 22-08/servidor.py
+++ metas/sessao 22-08/servidor.py
@@ -7,24 +7,24 @@ def start_server(host='localhost', port=8080):
     # associando o socket ao endereço da porta 
     socket_server.bind((host, port))
     
-    # Inicia o servidor para aceitar conexões
+    # inicia o servidor para aceitar conexões
     socket_server.listen(5)
     print(f"ouvindo em {host} : {port}")
 
     while True:
-        # Aceita uma conexão
+        # aceita a conexão
         client_socket, client_address = socket_server.accept()
         print(f"conectado com {client_address}")
 
-        # Recebe dados do cliente
+        # recebe dados do cliente
         data = client_socket.recv(1024).decode()
         print(f"mensagem recebida: {data}")
 
-        # Envia uma resposta para o cliente
+        # envia um retorno para o cliente
         response = "mensagem recebida com sucesso"
         client_socket.send(response.encode())
 
-        # Fecha a conexão com o cliente
+        # fecha a conexão
         client_socket.close()
 
 if __name__ == "__main__":
